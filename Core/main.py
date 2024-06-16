0901b7926af43438b3adf682b83a933c05ff47f2
I'm Pen <88584656+Pencoding1@users.noreply.github.com>
2024-06-16 19:11:44 +0700

    Change code in python
    Make it run on flask and python code. Change directory to make sure it works

---
+++ Core/main.py
@@ -0,0 +1,14 @@
+from flask import *
+import os
+template_path = os.path.join(os.getcwd(), "templates")
+static_path = os.path.join(os.getcwd(), "static")
+
+app = Flask(__name__, template_folder=template_path, static_folder=static_path)
+
+@app.route("/")
+@app.route("/home")
+def home():
+   return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
